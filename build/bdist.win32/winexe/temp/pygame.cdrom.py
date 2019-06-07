0bc65051dfa948c81998b7bb6f73680216d664cf
alexmuhl-r <51131246+alexmuhl-r@users.noreply.github.com>
2019-06-07 16:11:06 +0100

    Add files via upload

---
+++ build/bdist.win32/winexe/temp/pygame.cdrom.py
@@ -0,0 +1,10 @@
+
+def __load():
+    import imp, os, sys
+    dirname = sys.prefix
+    path = os.path.join(dirname, 'cdrom.pyd')
+    #print "py2exe extension module", __name__, "->", path
+    mod = imp.load_dynamic(__name__, path)
+##    mod.frozen = 1
+__load()
+del __load
